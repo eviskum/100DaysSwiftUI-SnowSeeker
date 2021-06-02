fd7dffc2a7d3097291744e7c7b9bed3372e962b4
Esben Viskum <viskum@viskum.dk>
2021-06-02 18:51:15 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Esben Viskum on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
